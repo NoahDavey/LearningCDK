48b904788836df0409f8bcd0f67cbcfecb1d7038
Noah Davey <noah.davey@gmail.com>
2021-06-21 21:11:19 +1000

    Experiment with lambda

--- lib/learning-cdk-stack.ts
+++ lib/learning-cdk-stack.ts
@@ -1,18 +1,31 @@
 import * as sns from '@aws-cdk/aws-sns';
 import * as subs from '@aws-cdk/aws-sns-subscriptions';
 import * as sqs from '@aws-cdk/aws-sqs';
+import * as lambda from '@aws-cdk/aws-lambda';
+import * as apigw from '@aws-cdk/aws-apigateway';
 import * as cdk from '@aws-cdk/core';
-
 export class LearningCdkStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'LearningCdkQueue', {
-      visibilityTimeout: cdk.Duration.seconds(300)
-    });
+    const hello = new lambda.Function(this, 'HelloHandler', {
+      runtime: lambda.Runtime.NODEJS_14_X,
+      code: lambda.Code.fromAsset('lambda'),
+      handler: 'hello.handler'
+    })
 
-    const topic = new sns.Topic(this, 'LearningCdkTopic');
+    new apigw.LambdaRestApi(this, 'Endpoint', {
+      handler: hello
+    })
 
-    topic.addSubscription(new subs.SqsSubscription(queue));
+    // const queue = new sqs.Queue(this, 'LearningCdkQueue', {
+    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // });
+
+    // const topic = new sns.Topic(this, 'LearningCdkTopic');
+
+    // topic.addSubscription(new subs.SqsSubscription(queue));
+
+
   }
 }
