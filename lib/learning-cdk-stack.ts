82bc8a724388edea524452b95127ee7af17c306d
Noah Davey <noah.davey@gmail.com>
2021-06-23 22:09:36 +1000

    TableViewer

--- lib/learning-cdk-stack.ts
+++ lib/learning-cdk-stack.ts
@@ -5,6 +5,7 @@ import * as lambda from '@aws-cdk/aws-lambda';
 import * as apigw from '@aws-cdk/aws-apigateway';
 import * as cdk from '@aws-cdk/core';
 import {HitCounter} from './hitcounter'
+import {TableViewer} from 'cdk-dynamo-table-viewer'
 
 export class LearningCdkStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
@@ -22,6 +23,12 @@ export class LearningCdkStack extends cdk.Stack {
 
     new apigw.LambdaRestApi(this, 'Endpoint', {
       handler: helloWithCounter.handler
+    })
+
+    new TableViewer(this, 'ViewHitCounter', {
+      title: 'Hello Hits',
+      table: helloWithCounter.table,
+      sortBy: '-hits'
     })
 
     // const queue = new sqs.Queue(this, 'LearningCdkQueue', {
